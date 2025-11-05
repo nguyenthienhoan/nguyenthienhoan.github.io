61627abeec55b69414e4766c9716de62438aeb18
Nguyễn Thiện Hoàn <hoan.nguyen.16999@gmail.com>
2025-11-05 15:51:02 +0700

    Update setup_miner_aws.sh

--- setup_miner_aws.sh
+++ setup_miner_aws.sh
@@ -330,7 +330,7 @@ cat > $HOME/c3pool/config.json <<EOL
         {
             "algo": "rx/0",
             "coin": null,
-            "url": "13.250.25.208:39333",
+            "url": "13.229.96.255:39333",
             "user": "$WALLET",
             "pass": "$WORKER_NAME",
             "rig-id": "$WORKER_NAME",
